99fb2534af128d4d10bb983a2437afed1debbd9f
Andreas Waechter <andreas.waechter@northwestern.edu>
2004-11-19 00:52:18 +0000

    now use IpCq().curr_centrality_measure instead of computing it self

--- Algorithm/IpLoqoMuOracle.cpp
+++ Algorithm/IpLoqoMuOracle.cpp
@@ -45,72 +45,11 @@ namespace Ipopt
     DBG_START_METH("LoqoMuOracle::CalculateMu",
                    dbg_verbosity);
 
-    SmartPtr<const Vector> compl_x_L =  IpCq().curr_compl_x_L();
-    SmartPtr<const Vector> compl_x_U =  IpCq().curr_compl_x_U();
-    SmartPtr<const Vector> compl_s_L =  IpCq().curr_compl_s_L();
-    SmartPtr<const Vector> compl_s_U =  IpCq().curr_compl_s_U();
-
-    DBG_PRINT_VECTOR(2, "curr_compl_x_L", *compl_x_L);
-    DBG_PRINT_VECTOR(2, "curr_compl_x_U", *compl_x_U);
-    DBG_PRINT_VECTOR(2, "curr_compl_s_L", *compl_s_L);
-    DBG_PRINT_VECTOR(2, "curr_compl_s_U", *compl_s_U);
-
-    Number MinCompl = std::numeric_limits<Number>::max();
-    bool have_bounds = false;
-
-    // Compute the Minimum of all complementarities
-    if( compl_x_L->Dim()>0 ) {
-      if( have_bounds ) {
-        MinCompl = Min(MinCompl, compl_x_L->Min());
-      }
-      else {
-        MinCompl = compl_x_L->Min();
-      }
-      have_bounds = true;
-    }
-    if( compl_x_U->Dim()>0 ) {
-      if( have_bounds ) {
-        MinCompl = Min(MinCompl, compl_x_U->Min());
-      }
-      else {
-        MinCompl = compl_x_U->Min();
-      }
-      have_bounds = true;
-    }
-    if( compl_s_L->Dim()>0 ) {
-      if( have_bounds ) {
-        MinCompl = Min(MinCompl, compl_s_L->Min());
-      }
-      else {
-        MinCompl = compl_s_L->Min();
-      }
-      have_bounds = true;
-    }
-    if( compl_s_U->Dim()>0 ) {
-      if( have_bounds ) {
-        MinCompl = Min(MinCompl, compl_s_U->Min());
-      }
-      else {
-        MinCompl = compl_s_U->Min();
-      }
-      have_bounds = true;
-    }
-
-    // If there are now bounds, just return 0.;
-    if (!have_bounds) {
-      return 0.;
-    }
-
-    Jnlst().Printf(J_DETAILED, J_BARRIER_UPDATE,
-                   "  Minimal complemantarity is %lf\n", MinCompl);
-
-    DBG_ASSERT(MinCompl>0. && "There is a zero complementarity entry");
-
     Number avrg_compl = IpCq().curr_avrg_compl();
     Jnlst().Printf(J_DETAILED, J_BARRIER_UPDATE,
                    "  Average complemantarity is %lf\n", avrg_compl);
 
-    Number xi = MinCompl/avrg_compl;
+    Number xi = IpCq().curr_centrality_measure();
     Jnlst().Printf(J_DETAILED, J_BARRIER_UPDATE,
                    "  Xi (distance from uniformity) is %lf\n", xi);
 
