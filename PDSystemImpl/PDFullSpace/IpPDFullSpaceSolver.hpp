50f54231d3dc778cf683c2f9ea02eb3e8a7bb0a8
Andreas Waechter <w4chtel@gmail.com>
2004-11-26 03:02:53 +0000

    a number of changes to include heuristics to handle numerically difficult linear systems (including asking linear solver to improve quality of solutions)
    svn-id: 100 @trunk

--- PDSystemImpl/PDFullSpace/IpPDFullSpaceSolver.hpp
+++ PDSystemImpl/PDFullSpace/IpPDFullSpaceSolver.hpp
@@ -17,6 +17,16 @@ namespace Ipopt
 
   /** This is the implemetation of the Primal-Dual System, using the
    *  full space approach with a direct linear solver.
+   *
+   *  A note on the iterative refinement: We perform at least
+   *  num_min_iter_ref number of iterative refinement steps.  If after
+   *  one iterative refinement the quality of the solution (defined in
+   *  ResidualRatio) does not improve or the maximal number of
+   *  iterative refinement steps is exceeded before the tolerance
+   *  residual_ratio_max_ is satisfied, we first ask the linear solver
+   *  to solve the system more accurately (e.g. by increasing the
+   *  pivot tolerance).  If that doesn't help or is not possible, we
+   *  treat the system, as if it is singular (i.e. increase delta's).
    */
   class PDFullSpaceSolver: public PDSystemSolver
   {
@@ -96,14 +106,29 @@ namespace Ipopt
     Number delta_c_last_;
     /** The last nonzero value for delta_d */
     Number delta_d_last_;
+    /** Flag indicating if for the current matrix the solution quality
+     *  of the augmented system solver has already been increased. */
+    bool augsys_improved_;
     //@}
 
     /** @name Parameters */
     //@{
     /** Minimal number of iterative refinement performed per backsolve */
     Index num_min_iter_ref_;
+    /** Maximal number of iterative refinement performed per backsolve */
+    Index num_max_iter_ref_;
     /** Maximal value for the regularization. */
     Number delta_regu_max_;
+    /** Maximal allowed ratio of the norm of the residual over the
+     *  norm of the right hand side and solution. */
+    Number residual_ratio_max_;
+    /** If the residual_ratio is larger than this value after trying
+     *  to improve the solution, the linear system is assumed to be
+     *  singular and modified. */
+    Number residual_ratio_singular_;
+    /** Factor defining require improvement to consider iterative
+     *  refinement successful. */
+    Number residual_improvement_factor_;
     //@}
 
     /** Internal function for a single backsolve (which will be used
@@ -112,7 +137,9 @@ namespace Ipopt
      *  solved (e.g. when the regularization parameter becomes too
      *  large.)
      */
-    bool SolveOnce(const SymMatrix& W,
+    bool SolveOnce(bool resolve_unmodified,
+                   bool pretend_singular,
+                   const SymMatrix& W,
                    const Matrix& J_c,
                    const Matrix& J_d,
                    const Matrix& Px_L,
@@ -194,6 +221,34 @@ namespace Ipopt
                           Vector& resid_zU,
                           Vector& resid_vL,
                           Vector& resid_vU);
+
+    /** Internal function for computing the ratio of the residual
+     *  compared to the right hand side and solution.  The smaller
+     *  this value, the better the solution. */
+    Number ComputeResidualRatio(const Vector& rhs_x,
+                                const Vector& rhs_s,
+                                const Vector& rhs_c,
+                                const Vector& rhs_d,
+                                const Vector& rhs_zL,
+                                const Vector& rhs_zU,
+                                const Vector& rhs_vL,
+                                const Vector& rhs_vU,
+                                const Vector& res_x,
+                                const Vector& res_s,
+                                const Vector& res_c,
+                                const Vector& res_d,
+                                const Vector& res_zL,
+                                const Vector& res_zU,
+                                const Vector& res_vL,
+                                const Vector& res_vU,
+                                const Vector& resid_x,
+                                const Vector& resid_s,
+                                const Vector& resid_c,
+                                const Vector& resid_d,
+                                const Vector& resid_zL,
+                                const Vector& resid_zU,
+                                const Vector& resid_vL,
+                                const Vector& resid_vU);
 
     /** @name Auxilliary functions */
     //@{
