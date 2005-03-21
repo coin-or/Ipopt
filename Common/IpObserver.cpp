bdf77c4f4b8dbae7685e2715c0c07eb3b18df410
Andreas Waechter <w4chtel@gmail.com>
2005-03-21 16:08:32 +0000

    - inline constructor and destructor - avoid DBG_START_METH
    svn-id: 233 @trunk

--- Common/IpObserver.cpp
+++ Common/IpObserver.cpp
@@ -10,45 +10,8 @@
 
 namespace Ipopt
 {
-
+#ifdef IP_DEBUG_OBSERVER
   const Index Observer::dbg_verbosity = 0;
   const Index Subject::dbg_verbosity = 0;
-
-  Observer::Observer()
-  {
-    DBG_START_METH("Observer::Observer", dbg_verbosity);
-  }
-
-  Observer::~Observer()
-  {
-    DBG_START_METH("Observer::~Observer", dbg_verbosity);
-    // Detach all subjects
-    for (Index i=0; i<(Index)subjects_.size(); i++) {
-      DBG_PRINT((1,"subjects_[%d] = 0x%x\n", i, subjects_[i]));
-    }
-    for (Int i=(Int)(subjects_.size()-1); i>=0; i--) {
-      DBG_PRINT((1, "Whatever\n"));
-      DBG_ASSERT(i >= 0);
-      if (i < 0) {
-        DBG_PRINT((1,"I had better get here...\n"));
-      }
-      DBG_PRINT((1,"About to detach subjects_[%d] = 0x%x\n", i, subjects_[i]));
-      RequestDetach(NT_All, subjects_[i]);
-    }
-  }
-
-  Subject::Subject()
-  {
-    DBG_START_METH("Subject::Subject", dbg_verbosity);
-  }
-
-  Subject::~Subject()
-  {
-    DBG_START_METH("Subject::~Subject", dbg_verbosity);
-    std::vector<Observer*>::iterator iter;
-    for (iter = observers_.begin(); iter != observers_.end(); iter++) {
-      (*iter)->ProcessNotification(Observer::NT_BeingDestroyed, this);
-    }
-  }
-
+#endif
 } // namespace Ipopt
