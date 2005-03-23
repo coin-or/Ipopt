a8215257a44587aa707fdbaaf62df8c9e26d5c46
Andreas Waechter <w4chtel@gmail.com>
2005-03-23 06:56:56 +0000

    don't return huge multiplier values for fixed variables
    svn-id: 243 @trunk

--- LinAlgImpl/Serial/IpTNLPAdapter.cpp
+++ LinAlgImpl/Serial/IpTNLPAdapter.cpp
@@ -755,8 +755,8 @@ namespace Ipopt
     Number* full_z_L = new Number[n_full_x_];
     Number* full_z_U = new Number[n_full_x_];
     for (int i=0; i<n_full_x_; i++) {
-      full_z_L[i] = nlp_lower_bound_inf_;
-      full_z_U[i] = nlp_upper_bound_inf_;
+      full_z_L[i] = 0.; // ToDo nlp_lower_bound_inf_;
+      full_z_U[i] = 0.; // ToDo nlp_upper_bound_inf_;
     }
     ResortBnds(z_L, full_z_L, z_U, full_z_U);
 
