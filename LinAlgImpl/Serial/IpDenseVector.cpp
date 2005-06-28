280cba39e4d490228e9a4cf229cd114fb78a1bd2
Andreas Waechter <andreas.waechter@northwestern.edu>
2005-06-28 22:29:55 +0000

    astyle

--- LinAlgImpl/Serial/IpDenseVector.cpp
+++ LinAlgImpl/Serial/IpDenseVector.cpp
@@ -114,9 +114,9 @@ namespace Ipopt
       }
       else {
         if (dense_x->homogeneous_) {
-	  if (dense_x->scalar_!=0.) {
-	    IpBlasDaxpy(Dim(), alpha, &dense_x->scalar_, 0, values_, 1);
-	  }
+          if (dense_x->scalar_!=0.) {
+            IpBlasDaxpy(Dim(), alpha, &dense_x->scalar_, 0, values_, 1);
+          }
         }
         else {
           IpBlasDaxpy(Dim(), alpha, dense_x->values_, 1, values_, 1);
