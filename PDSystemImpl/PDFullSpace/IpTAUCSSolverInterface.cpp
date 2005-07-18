37a553a4fce5caec855fa5439b624b2d713ab26f
Andreas Waechter <andreas.waechter@northwestern.edu>
2005-07-18 19:14:09 +0000

    added TAUCS interface, written by Yifan Hu (Wolfram Research)  received CSRO on Jul 03, 2005

---
+++ PDSystemImpl/PDFullSpace/IpTAUCSSolverInterface.cpp
@@ -0,0 +1,346 @@
+// Copyright (C) 2005, Yifan Hu (Wolfram Research)
+// All Rights Reserved.
+// This code is published under the Common Public License.
+//
+// $Id: IpPardisoSolverInterface.cpp 304 2005-05-25 19:17:41Z andreasw $
+//
+// Author:  Yifan Hu              2005-04-06
+
+#include "IpTAUCSSolverInterface.hpp"
+
+#ifdef OLD_C_HEADERS
+# include <math.h>
+#else
+# include <cmath>
+#endif
+
+namespace Ipopt
+{
+
+  DBG_SET_VERBOSITY(0);
+
+  TAUCSSolverInterface::TAUCSSolverInterface()
+      :
+      m(0),
+      n(0),
+      nz(0),
+      initialized(false),
+      negevals(-1),
+      a(NULL),
+      multi_frontal(false)
+  {
+    DBG_START_METH("TAUCSSolverInterface::TAUCSSolverInterface()",dbg_verbosity);
+
+  }
+
+  static void taucs_factor_delete_L(taucs_factorization *F)
+  {
+    if (F->type == TAUCS_FACTORTYPE_LLT_SUPERNODAL)
+      taucs_supernodal_factor_free(F->L);
+    if (F->type == TAUCS_FACTORTYPE_LLT_CCS)
+      taucs_ccs_free((taucs_ccs_matrix*) F->L);
+  }
+
+  static void taucs_delete(taucs_factorization *F, taucs_ccs_matrix *A)
+  {
+    if (F) {
+      //printf("MEM: freeing factor and colptr and rowptr\n");
+      taucs_factor_delete_L(F);
+      taucs_free(F->rowperm);
+      taucs_free(F->colperm);
+      taucs_free(F);
+      F = NULL;
+
+    }
+    //printf("MEM: freeing A\n");
+    if (A) {
+      taucs_ccs_free(A);
+      A = NULL;
+    }
+  }
+
+  TAUCSSolverInterface::~TAUCSSolverInterface()
+  {
+    DBG_START_METH("TAUCSSolverInterface::~TAUCSSolverInterface()",
+                   dbg_verbosity);
+
+    // Tell TAUCS to release all memory
+    if (initialized) {
+      taucs_delete(taucs_factor, A);
+      //         DBG_ASSERT(ERROR==0);
+    }
+  }
+
+  bool TAUCSSolverInterface::InitializeImpl(const OptionsList& options,
+      const std::string& prefix)
+  {
+    Number value = 0.0;
+
+    // Tell TAUCS to release all memory if it had been used before
+    if (initialized) {
+      taucs_delete(taucs_factor, A);
+      //   DBG_ASSERT(ERROR==0);
+    }
+
+    // Reset all private data
+    m = n = 0;
+    nz =0;
+    initialized =false;
+
+    multi_frontal = false;     /* we try no multifront for now */
+
+    A = NULL;
+
+    taucs_factor = NULL;
+
+    // Call TAUCS's initialization routine
+
+    return true;
+  }
+
+  ESymSolverStatus TAUCSSolverInterface::MultiSolve(bool new_matrix,
+      const Index* ia,
+      const Index* ja,
+      Index nrhs,
+      double* rhs_vals,
+      bool check_NegEVals,
+      Index numberOfNegEVals)
+  {
+    DBG_START_METH("TAUCSSolverInterface::MultiSolve",dbg_verbosity);
+    DBG_ASSERT(!check_NegEVals || ProvidesInertia());
+    DBG_ASSERT(initialized);
+
+    // check if a factorization has to be done
+    if (new_matrix) {
+      //delete old factor
+      //printf("MEM: new matrix delete L\n");
+      taucs_factor_delete_L(taucs_factor);
+
+      // perform the factorization
+      ESymSolverStatus retval;
+      retval = Factorization(ia, ja, check_NegEVals, numberOfNegEVals);
+      if (retval!=SYMSOLVER_SUCCESS) {
+        DBG_PRINT((1, "FACTORIZATION FAILED!\n"));
+        return retval;  // Matrix singular or error occurred
+      }
+    }
+
+    // do the solve
+    return Solve(ia, ja, nrhs, rhs_vals);
+  }
+
+  double* TAUCSSolverInterface::GetValuesArrayPtr()
+  {
+    DBG_ASSERT(initialized);
+    DBG_ASSERT(a);
+    return a;
+  }
+
+  /** Initialize the local copy of the positions of the nonzero
+      elements */
+  ESymSolverStatus TAUCSSolverInterface::InitializeStructure
+  (Index dim, Index nonzeros,
+   const Index* ia,
+   const Index* ja)
+  {
+    DBG_START_METH("TAUCSSolverInterface::InitializeStructure",dbg_verbosity);
+    m = n = dim;
+    nz = nonzeros;
+
+    // create TAUCS's matrix
+    //      A = (taucs_ccs_matrix*) taucs_malloc(sizeof(taucs_ccs_matrix));
+    //printf("MEM: creating ccs matrix A\n");
+    A = taucs_ccs_create(m, n, nz, TAUCS_SYMMETRIC | TAUCS_DOUBLE | TAUCS_LOWER);
+    a = A->values.d;
+    for (int i = 0; i <= m; i++)
+      A->colptr[i] = (Index) ia[i];
+    for (int i = 0; i < nz; i++)
+      A->rowind[i] = (Index) ja[i];
+
+    // Do the symbolic facotrization
+    ESymSolverStatus retval = SymbolicFactorization(ia, ja);
+    if (retval != SYMSOLVER_SUCCESS) {
+      return retval;
+    }
+
+    initialized = true;
+
+    return retval;
+  }
+
+  ESymSolverStatus
+  TAUCSSolverInterface::SymbolicFactorization(const Index* ia,
+      const Index* ja)
+  {
+    double opt_maxdepth  = 0.0; /* default meaning no limit */
+    int* rowperm = NULL, *colperm = NULL;
+    taucs_ccs_matrix *PAPT = NULL;
+
+    ESymSolverStatus retcode = SYMSOLVER_SUCCESS;
+    DBG_START_METH("TAUCSSolverInterface::SymbolicFactorization",
+                   dbg_verbosity);
+
+    //generate a TAUCS matrix
+
+    //taucs factorization structure
+    //printf("MEM: allocing taucs_factor\n");
+    taucs_factor = (taucs_factorization*) taucs_malloc(sizeof(taucs_factorization));
+    if (!taucs_factor) {
+      taucs_printf("MEM: taucs_factor: memory allocation\n");
+      retcode = SYMSOLVER_FATAL_ERROR;
+      goto release_and_return;
+    }
+    taucs_factor->n       = A->n;
+    taucs_factor->type    = TAUCS_FACTORTYPE_NONE;
+    taucs_factor->flags   = A->flags; /* remember data type */
+
+    // Call TAUCS to do the analysis phase
+    //printf("MEM: rowperm, colperm\n");
+    taucs_ccs_order(A,&rowperm,&colperm, "metis");
+    if (!rowperm) {
+      Jnlst().Printf(J_ERROR, J_LINEAR_ALGEBRA,
+                     "Error during ordering in TAUCS analysis phase.  ERROR = %d.\n",
+                     SYMSOLVER_FATAL_ERROR);
+      retcode = SYMSOLVER_FATAL_ERROR;
+      goto release_and_return;
+    }
+    taucs_factor->rowperm = rowperm;
+    taucs_factor->colperm = colperm;
+
+    /* permute the matrix */
+    //printf("MEM: making PAPT\n");
+    PAPT = taucs_ccs_permute_symmetrically(A,rowperm,colperm);
+    if (!PAPT) {
+      Jnlst().Printf(J_ERROR, J_LINEAR_ALGEBRA,
+                     "Error during permutation in TAUCS analysis phase.  ERROR = %d.\n",
+                     SYMSOLVER_FATAL_ERROR);
+      retcode = SYMSOLVER_FATAL_ERROR;
+      goto release_and_return;
+    }
+
+    // symbolic factorization
+    if (multi_frontal) {
+      taucs_factor->L = taucs_ccs_factor_ldlt_symbolic_maxdepth(PAPT,(int) opt_maxdepth);
+      if (!(taucs_factor->L)) {
+        Jnlst().Printf(J_ERROR, J_LINEAR_ALGEBRA,
+                       "Error during permutation in TAUCS analysis phase.  ERROR = %d.\n",
+                       SYMSOLVER_FATAL_ERROR);
+        retcode = SYMSOLVER_FATAL_ERROR;
+        goto release_and_return;
+      }
+    }
+    else {
+      /* for this case TAUCS does not provide separate symbolic and numerical
+         factorization routines */
+    }
+
+    taucs_factor->type = TAUCS_FACTORTYPE_IND;
+
+    //printf("MEM: freeing PAPT\n");
+    taucs_ccs_free(PAPT);
+    return retcode;
+release_and_return:
+                      taucs_free(rowperm);
+    taucs_free(colperm);
+    taucs_ccs_free(PAPT);
+    return retcode;
+
+
+  }
+
+  ESymSolverStatus
+  TAUCSSolverInterface::Factorization(const Index* ia,
+                                      const Index* ja,
+                                      bool check_NegEVals,
+                                      Index numberOfNegEVals)
+  {
+    taucs_ccs_matrix *PAPT = NULL;
+
+    DBG_START_METH("TAUCSSolverInterface::Factorization",dbg_verbosity);
+
+    PAPT = taucs_ccs_permute_symmetrically(A,taucs_factor->rowperm,
+                                           taucs_factor->colperm);
+
+    /* numerical factorization */
+    int rc = 0;
+    if (multi_frontal) {
+      rc = taucs_ccs_factor_ldlt_numeric(PAPT, taucs_factor->L);
+    }
+    else {
+
+      double opt_maxdepth  = 0.0; /* default meaning no limit */
+      taucs_factor->L = taucs_ccs_factor_ldlt_ll_maxdepth(PAPT,(int) opt_maxdepth);
+
+    }
+    taucs_ccs_free(PAPT);
+
+    if (!(taucs_factor->L) || rc) {
+      Jnlst().Printf(J_ERROR, J_LINEAR_ALGEBRA,
+                     "Error TAUCS factorizarion phase.  ERROR = %d.\n",
+                     SYMSOLVER_FATAL_ERROR);
+      return SYMSOLVER_FATAL_ERROR;
+    }
+
+
+    int inertia[3];
+    for (int i = 0; i < 3; i++)
+      inertia[i] = 0;
+    taucs_inertia_calc(taucs_factor->L,inertia);
+    negevals = (Index) inertia[2];
+
+
+    // Check whether the number of negative eigenvalues matches the requested
+    // count: what do we do with TAUCS????
+    if (check_NegEVals && (numberOfNegEVals!=negevals)) {
+      return SYMSOLVER_WRONG_INERTIA;
+    }
+
+    return SYMSOLVER_SUCCESS;
+  }
+
+  ESymSolverStatus TAUCSSolverInterface::Solve(const Index* ia,
+      const Index* ja,
+      Index nrhs,
+      double *rhs_vals)
+  {
+    int j;
+    DBG_START_METH("TAUCSSolverInterface::Solve",dbg_verbosity);
+
+    //printf("MEM: allocing px and pb\n");
+    void *PX = (void*) taucs_malloc(element_size(A->flags)*nrhs*(A->n));
+    void *PB = (void*) taucs_malloc(element_size(A->flags)*nrhs*(A->n));
+
+
+    int ld = (A->n) * element_size(A->flags);
+    for (j=0; j<nrhs; j++)
+      taucs_vec_permute (A->n, A->flags,(char*)rhs_vals+j*ld,(char*)PB+j*ld,
+                         taucs_factor->rowperm);
+
+    taucs_supernodal_solve_ldlt_many(taucs_factor->L, nrhs, PX, A->n, PB, ld);
+
+    for (j=0; j<nrhs; j++) {
+      taucs_vec_ipermute(A->n, A->flags, (char*)PX+j*ld, (char*) rhs_vals+j*ld,
+                         taucs_factor->rowperm);
+    }
+
+    //printf("MEM: freeing pb and px\n");
+    taucs_free(PB);
+    taucs_free(PX);
+
+    return SYMSOLVER_SUCCESS;
+  }
+
+  Index TAUCSSolverInterface::NumberOfNegEVals() const
+  {
+    DBG_START_METH("TAUCSSolverInterface::NumberOfNegEVals",dbg_verbosity);
+    DBG_ASSERT(negevals>=0);
+    return negevals;
+  }
+
+  bool TAUCSSolverInterface::IncreaseQuality()
+  {
+    // At the moment, I don't see how we could tell TAUCS to do better
+    return false;
+  }
+
+} // namespace Ipopt
---
+++ PDSystemImpl/PDFullSpace/IpTAUCSSolverInterface.hpp
@@ -0,0 +1,287 @@
+// Copyright (C) 2005, Yifan Hu (Wolfram Research)
+// All Rights Reserved.
+// This code is published under the Common Public License.
+//
+// $Id: IpPardisoSolverInterface.cpp 304 2005-05-25 19:17:41Z andreasw $
+//
+// Author:  Yifan Hu              2005-04-06
+
+#ifndef __IPTAUCSSOLVERINTERFACE_HPP__
+#define __IPTAUCSSOLVERINTERFACE_HPP__
+
+#include "IpSparseSymLinearSolverInterface.hpp"
+
+#include <stdlib.h>
+
+extern "C"
+{
+  // we can not just wrap a extern C around
+  // #include "taucs.h"
+  // becuase we will get tons of messages and errors,
+  // because taucs.h includes a lot of C libraried.
+  typedef double    taucs_double;
+  typedef float     taucs_single;
+  //we do not use complex anyway, so do worry about
+  // whether TAUCS_C99_COMPLEX is defined or not
+#ifdef TAUCS_C99_COMPLEX
+#include <complex.h>
+
+  typedef _Complex double taucs_dcomplex;
+  typedef _Complex float  taucs_scomplex;
+#else /* C99 */
+
+  typedef struct {
+    double r,i;
+  }
+  taucs_dcomplex;
+  typedef struct {
+    float  r,i;
+  }
+  taucs_scomplex;
+#endif
+#define TAUCS_INT       1024
+#define TAUCS_DOUBLE    2048
+#define TAUCS_SINGLE    4096
+#define TAUCS_DCOMPLEX  8192
+#define TAUCS_SCOMPLEX 16384
+#define TAUCS_LOWER      1
+#define TAUCS_UPPER      2
+#define TAUCS_TRIANGULAR 4
+#define TAUCS_SYMMETRIC  8
+#define TAUCS_HERMITIAN  16
+#define TAUCS_PATTERN    32
+#define TAUCS_FACTORTYPE_NONE           0
+
+  typedef struct {
+    int   n;
+    int   flags;
+    int   type;
+    int*  rowperm;
+    int*  colperm;
+    void* L;
+  }
+  taucs_factorization;
+  typedef struct {
+    int     n;    /* columns                      */
+    int     m;    /* rows; don't use if symmetric   */
+    int     flags;
+    int*    colptr; /* pointers to where columns begin in rowind and values. */
+    /* 0-based. Length is (n+1). */
+    int*    rowind; /* row indices */
+
+    union {
+      void*           v;
+      taucs_double*   d;
+      taucs_single*   s;
+      taucs_dcomplex* z;
+      taucs_scomplex* c;
+    } values;
+
+  }
+  taucs_ccs_matrix;
+
+
+  void  taucs_free_stub(void *ptr);
+  void  taucs_free   (void* ptr);
+#define taucs_free(x)      taucs_free_stub(x)
+  /*
+   extern void iFree(void *bp);
+  #define taucs_free(x)  iFree(x)
+  */
+
+
+  void* taucs_malloc_stub(size_t size);
+  void* taucs_malloc (size_t size);
+#define taucs_malloc(x)    taucs_malloc_stub(x);
+  /*
+   extern void *MallocAlign16( size_t size);
+  #define taucs_malloc(x)    MallocAlign16(x)
+  */
+
+  int taucs_printf(char *fmt, ...);
+  void taucs_ccs_free(taucs_ccs_matrix* matrix);
+  void taucs_ccs_order(taucs_ccs_matrix* matrix,
+                       int** perm, int** invperm,
+                       char* which);
+  void taucs_vec_permute(int n, int flags, void* v, void* pv, int p[]);
+  void    taucs_inertia_calc(void* vL, int* inertia);
+  void taucs_supernodal_factor_free                (void* L);
+  void taucs_supernodal_factor_free_numeric        (void* L);
+  int   taucs_ccs_factor_ldlt_numeric(taucs_ccs_matrix* A,void* L);
+  taucs_ccs_matrix* taucs_ccs_permute_symmetrically (taucs_ccs_matrix* A,
+      int* perm, int* invperm);
+  int taucs_supernodal_solve_ldlt_many(void *L,int n,void* X, int ld_X,void* B,
+                                       int ld_B);
+  void* taucs_ccs_factor_ldlt_ll_maxdepth(taucs_ccs_matrix* A,int max_depth);
+  void* taucs_ccs_factor_ldlt_symbolic_maxdepth(taucs_ccs_matrix* A,int max_depth);
+  void taucs_vec_ipermute(int n, int flags, void* v, void* pv, int p[]);
+  taucs_ccs_matrix* taucs_ccs_create(int m, int n, int nnz, int flags);
+
+}
+
+static int element_size(int flags)
+{
+  if (flags & TAUCS_SINGLE)
+    return sizeof(taucs_single);
+  if (flags & TAUCS_DOUBLE)
+    return sizeof(taucs_double);
+  if (flags & TAUCS_SCOMPLEX)
+    return sizeof(taucs_scomplex);
+  if (flags & TAUCS_DCOMPLEX)
+    return sizeof(taucs_dcomplex);
+  if (flags & TAUCS_INT)
+    return sizeof(int);
+  return -1;
+}
+
+/*********************************************************/
+/* Generic Factor routines                               */
+/* (Experimental, unstable interface)                    */
+/*********************************************************/
+
+#define TAUCS_FACTORTYPE_NONE           0
+#define TAUCS_FACTORTYPE_LLT_SUPERNODAL 1
+#define TAUCS_FACTORTYPE_LLT_CCS        2
+#define TAUCS_FACTORTYPE_LDLT_CCS       3
+#define TAUCS_FACTORTYPE_LLT_OOC        4
+#define TAUCS_FACTORTYPE_LU_OOC         5
+#define TAUCS_FACTORTYPE_IND            6
+#define TAUCS_FACTORTYPE_IND_OOC        7
+#define TAUCS_FACTORTYPE_LU             8
+
+
+namespace Ipopt
+{
+
+  /** Interface to the linear solver TAUCS, derived from
+   *  SparseSymLinearSolverInterface.  For details, see description of
+   *  SparseSymLinearSolverInterface base class.
+   */
+  class TAUCSSolverInterface: public SparseSymLinearSolverInterface
+  {
+  public:
+    /** @name Constructor/Destructor */
+    //@{
+    /** Constructor */
+    TAUCSSolverInterface();
+
+    /** Destructor */
+    virtual ~TAUCSSolverInterface();
+    //@}
+
+    /** overloaded from AlgorithmStrategyObject */
+    bool InitializeImpl(const OptionsList& options,
+                        const std::string& prefix);
+
+
+    /** @name Methods for requesting solution of the linear system. */
+    //@{
+    /** Method for initializing internal stuctures. */
+    virtual ESymSolverStatus InitializeStructure(Index dim, Index nonzeros,
+        const Index *ia,
+        const Index *ja);
+
+    /** Method returing an internal array into which the nonzero
+     *  elements are to be stored. */
+    virtual double* GetValuesArrayPtr();
+
+    /** Solve operation for multiple right hand sides. */
+    virtual ESymSolverStatus MultiSolve(bool new_matrix,
+                                        const Index* ia,
+                                        const Index* ja,
+                                        Index nrhs,
+                                        double* rhs_vals,
+                                        bool check_NegEVals,
+                                        Index numberOfNegEVals);
+
+    /** Number of negative eigenvalues detected during last
+     *  factorization.
+     */
+    virtual Index NumberOfNegEVals() const;
+    //@}
+
+    //* @name Options of Linear solver */
+    //@{
+    /** Request to increase quality of solution for next solve.
+     */
+    virtual bool IncreaseQuality();
+
+    /** Query whether inertia is computed by linear solver.
+     *  Returns true, if linear solver provides inertia.
+     */
+    virtual bool ProvidesInertia() const
+    {
+      return true;
+    }
+    /** Query of requested matrix type that the linear solver
+     *  understands.
+     */
+    EMatrixFormat MatrixFormat() const
+    {
+      return CSR_Format_0_Offset;
+    }
+    //@}
+
+  private:
+    /**@name Default Compiler Generated Methods
+     * (Hidden to avoid implicit creation/calling).
+     * These methods are not implemented and 
+     * we do not want the compiler to implement
+     * them for us, so we declare them private
+     * and do not define them. This ensures that
+     * they will not be implicitly created/called. */
+    //@{
+    /** Copy Constructor */
+    TAUCSSolverInterface(const TAUCSSolverInterface&);
+
+    /** Overloaded Equals Operator */
+    void operator=(const TAUCSSolverInterface&);
+    //@}
+
+    /** @name Information about the matrix */
+    //@{
+    /** Number of rows and columns of the matrix */
+    Index m, n;
+
+    /** Number of nonzeros of the matrix in triplet representation. */
+    Index nz;
+
+    /** Array for storing the values of the matrix. */
+    double* a;
+
+    /** @name Information about most recent factorization/solve */
+    /** Number of negative eigenvalues */
+    Index negevals;
+
+    /** @name Initialization flags */
+    /** Flag indicating if internal data is initialized.
+     *  For initialization, this object needs to have seen a matrix */
+    bool initialized;
+
+    /* whether to use multifrontal or IC indefinite LL factorization */
+    bool multi_frontal;
+
+    /* name Solver specific information */
+    taucs_factorization *taucs_factor;
+    taucs_ccs_matrix *A;
+
+    // Call TAUCS to do the analysis phase.
+    ESymSolverStatus SymbolicFactorization(const Index* ia,
+                                           const Index* ja);
+
+    // Call TAUCS to factorize the Matrix.
+    ESymSolverStatus Factorization(const Index* ia,
+                                   const Index* ja,
+                                   bool check_NegEVals,
+                                   Index numberOfNegEVals);
+
+    // Call TAUCS to do the Solve.
+    ESymSolverStatus Solve(const Index* ia,
+                           const Index* ja,
+                           Index nrhs,
+                           double *rhs_vals);
+    //@}
+  };
+
+} // namespace Ipopt
+#endif
