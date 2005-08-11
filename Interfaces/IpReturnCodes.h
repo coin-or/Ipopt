531603195246e36e3c4ec00f00ee81458e21f202
Andreas Waechter <andreas.waechter@northwestern.edu>
2005-08-11 17:25:53 +0000

    set svn properties

--- Interfaces/IpReturnCodes.h
+++ Interfaces/IpReturnCodes.h
@@ -3,7 +3,7 @@
 // All Rights Reserved.
 // This code is published under the Common Public License.
 //
-// $Id: IpIpoptApplication.hpp 430 2005-08-10 00:19:54Z andreasw $
+// $Id$
 //
 // Authors:  Carl Laird, Andreas Waechter     IBM    2004-08-13
 ************************************************************************/
