bfb3edbfcb30e97324178bc22e1b6c5af24615a6
Andreas Waechter <andreas.waechter@northwestern.edu>
2005-12-13 21:55:32 +0000

    If option with disallowed clobbering is now specified again, print  Warning message instead of aborting program.

--- Common/IpOptionsList.cpp
+++ Common/IpOptionsList.cpp
@@ -83,20 +83,32 @@ namespace Ipopt
       }
     }
 
-    if (will_allow_clobber(tag)) {
+    if (!will_allow_clobber(tag)) {
+      std::string msg = "WARNING: Option \"" + tag;
+      msg += " ";
+      msg += value;
+      msg += "\" is set to disallow clobbering.\n         Its previous value \"";
+      msg += options_[lowercase(tag)].GetValue();
+      msg += "\" is now overwritten.\n";
+      jnlst_->Printf(J_WARNING, J_MAIN, msg.c_str());
       OptionsList::OptionValue optval(lowercase(value), allow_clobber);
       options_[lowercase(tag)] = optval;
-      return true;
     }
+    else {
+      //    if (will_allow_clobber(tag)) {
+      OptionsList::OptionValue optval(lowercase(value), allow_clobber);
+      options_[lowercase(tag)] = optval;
+    }
+    return true;
 
-    std::string msg = "Option: \"" + tag;
-    msg += " ";
-    msg += value;
-    msg += "\" not taken because a value of \n\"" ;
-    msg += options_[lowercase(tag)].GetValue();
-    msg += "\" already exists and is set to disallow clobbering.\n\n";
-    jnlst_->Printf(J_ERROR, J_MAIN, msg.c_str());
-    return false;
+    //     std::string msg = "Option: \"" + tag;
+    //     msg += " ";
+    //     msg += value;
+    //     msg += "\" not taken because a value of \n\"" ;
+    //     msg += options_[lowercase(tag)].GetValue();
+    //     msg += "\" already exists and is set to disallow clobbering.\n\n";
+    //     jnlst_->Printf(J_ERROR, J_MAIN, msg.c_str());
+    //     return false;
   }
 
   bool OptionsList::SetNumericValue(const std::string& tag, Number value,
