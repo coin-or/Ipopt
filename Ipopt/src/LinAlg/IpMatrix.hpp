c17304289df536af9885daa7cc53e2d33d6a95b3
Andreas Waechter <w4chtel@gmail.com>
2007-07-03 01:03:28 +0000

    ran astyle
    svn-id: 1035 @trunk

--- Ipopt/src/LinAlg/IpMatrix.hpp
+++ Ipopt/src/LinAlg/IpMatrix.hpp
@@ -300,10 +300,10 @@ namespace Ipopt
    if (dbg_jrnl.Verbosity() >= (__verbose_level)) { \
       if (dbg_jrnl.Jnlst()!=NULL) { \
         (__mat).Print(dbg_jrnl.Jnlst(), \
-		      J_ERROR, J_DBG, \
-		      __mat_name, \
-		      dbg_jrnl.IndentationLevel()*2, \
-		      "# "); \
+        J_ERROR, J_DBG, \
+        __mat_name, \
+        dbg_jrnl.IndentationLevel()*2, \
+        "# "); \
       } \
    }
 #endif // #if COIN_IPOPT_VERBOSITY == 0
