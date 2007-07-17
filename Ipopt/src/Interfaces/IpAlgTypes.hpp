0df7ae65aea42d73049d410d1084b68d8b32c177
Andreas Waechter <w4chtel@gmail.com>
2007-07-17 23:09:07 +0000

    fix segfault for case with no variables and infeasible problems
    svn-id: 1054 @3.3

--- Ipopt/src/Interfaces/IpAlgTypes.hpp
+++ Ipopt/src/Interfaces/IpAlgTypes.hpp
@@ -45,6 +45,7 @@ namespace Ipopt
   DECLARE_STD_EXCEPTION(INVALID_WARMSTART);
   DECLARE_STD_EXCEPTION(INTERNAL_ABORT);
   DECLARE_STD_EXCEPTION(NO_FREE_VARIABLES_BUT_FEASIBLE);
+  DECLARE_STD_EXCEPTION(NO_FREE_VARIABLES_AND_INFEASIBLE);
   /** Exception FAILED_INITIALIZATION for problem during
    *  initialization of a strategy object (or other problems).  This
    *  is thrown by a strategy object, if a problem arises during
