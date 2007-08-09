c3f63d7e7796252ff289bbc9a60297a4aa1f1e88
Andreas Waechter <andreas.waechter@northwestern.edu>
2007-08-09 18:16:20 +0000

    Hack to fix a problem with MUMPS which otherwise in the serial version defines a global variable twice

--- trunk/Ipopt/src/Algorithm/LinearSolvers/IpMumpsSolverInterface.cpp
+++ trunk/Ipopt/src/Algorithm/LinearSolvers/IpMumpsSolverInterface.cpp
@@ -15,6 +15,9 @@
 extern "C"
 {
 #include "dmumps_c.h"
+  // The following line is a fix for otherwise twice-defined global variable
+  // (This would have to be taken out for a parallel MUMPS version!)
+#define MPI_COMM_WORLD IPOPT_MPI_COMM_WORLD
 #include "mpi.h"
 }
 
