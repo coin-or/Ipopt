10c9087f2492146a05e0bc6f336b72d1137e9eed
Andreas Waechter <w4chtel@gmail.com>
2008-05-30 18:40:58 +0000

    restored stable version number
    svn-id: 1251 @3.4

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.4.1"
+#define IPOPT_VERSION "3.4stable"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
