89df8b7101f008196a832e710126787a13328538
Stefan Vigerske <svigerske@gams.com>
2008-08-07 09:13:46 +0000

    add missing includes of cstdio to get compiling with (experimental) gcc 4.4

--- Ipopt/src/Algorithm/IpOrigIterationOutput.cpp
+++ Ipopt/src/Algorithm/IpOrigIterationOutput.cpp
@@ -18,6 +18,17 @@
 # endif
 #endif
 
+// for sprintf
+#ifdef HAVE_CSTDIO
+# include <cstdio>
+#else
+# ifdef HAVE_STDIO_H
+#  include <stdio.h>
+# else
+#  error "don't have header file for stdio"
+# endif
+#endif
+
 namespace Ipopt
 {
   OrigIterationOutput::OrigIterationOutput()
