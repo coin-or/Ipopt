902c7abc2002e1c8cf0da4a64e774399bcebbf95
Andreas Waechter <w4chtel@gmail.com>
2008-08-26 18:30:07 +0000

    restored version number for Windows
    svn-id: 1308 @3.5

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.5.1"
+#define IPOPT_VERSION "3.5stable"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
