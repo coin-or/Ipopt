38a435f0d61e65bf6476ad0e26a30d23ec4e61f0
Andreas Waechter <andreas.waechter@northwestern.edu>
2008-09-29 22:43:46 +0000

    prepared stable for new release (ChangeLog and version number for Windows)

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.5stable"
+#define IPOPT_VERSION "3.5.4"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
