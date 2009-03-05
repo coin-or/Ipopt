94938241d51eaf623ab0e25375553b73c22e7560
Peter Carbonetto <peter.carbonetto@gmail.com>
2009-03-05 23:14:06 +0000

    Made a few changes to the documentation for the MATLAB interface.

--- Ipopt/contrib/MatlabInterface/ipopt.m
+++ Ipopt/contrib/MatlabInterface/ipopt.m
@@ -16,12 +16,24 @@
 %     funcs.objective (required)
 %
 %     Calculates the objective function at the current point. It takes one
-%     point, the current iterate x.
+%     point, the current iterate x. For example, the definition of the
+%     objective function for the Hock & Schittkowski (H&S) test problem #71
+%     (with 4 optimization variables) would be
+% 
+%         function f = objective (x)
+%           f = x(1)*x(4)*sum(x(1:3)) + x(3);
 %         
 %     funcs.gradient (required)
 %
-%     Computes the gradient of the objective at the current point. It
-%     takes one input, the current iterate x.
+%     Computes the gradient of the objective at the current point. It takes
+%     one input, the current iterate x. For H&S test problem #71, the
+%     definition of the gradient callback would be
+%
+%         function g = gradient (x)
+%           g = [ x(1)*x(4) + x(4)*sum(x(1:3))
+%                 x(1)*x(4)
+%                 x(1)*x(4) + 1
+%                 x(1)*sum(x(1:3)) ]; 
 %
 %     funcs.constraints (optional)
 %
@@ -29,7 +41,11 @@
 %     variables. It evaluates the constraint functions at the current
 %     point. It takes one input, x. The return value is a vector of length
 %     equal to the number of constraints (it must be of the same length as
-%     options.cl and options.cu).
+%     options.cl and options.cu). For H&S test problem #71, the
+%     callback definition would be
+%
+%         function c = constraints (x)
+%           c = [ prod(x); sum(x.^2) ];
 %
 %     funcs.jacobian (optional)
 % 
@@ -38,14 +54,25 @@
 %     point. It takes one input, x. The output must always be an M x N
 %     sparse matrix, where M is the number of constraints and N is the
 %     number of variables. Type HELP SPARSE for more information on
-%     constructing sparse matrices in MATLAB.
+%     constructing sparse matrices in MATLAB. The definition of the
+%     callback function for H&S test problem #71 would be
+%
+%         function J = jacobian (x)
+%           sparse([ prod(x)./x; 2*x ]);
+%
+%     Notice that the return value is a sparse matrix.
 %
 %     funcs.jacobianstructure (optional)
 %
 %     This function is only required if there are constraints on your
 %     variables. It takes no inputs. The return value is a sparse
 %     matrix whereby an entry is nonzero if and only if the Jacobian of
-%     the constraints is nonzero at ANY point.
+%     the constraints is nonzero at ANY point. The callback function for
+%     the H&S test problem #71 simply returns a 2 x 4 matrix of ones in
+%     the sparse matrix format:
+%
+%         function J = jacobianstructure() 
+%           J = sparse(ones(2,4));
 %
 %     funcs.hessian (optional)
 %
@@ -66,12 +93,31 @@
 %     matrix, where N is the number of variables. In other words, if X is
 %     the output value, then X must be the same as TRIL(X).
 %
+%     Here is an implementation of the Hessian callback routine for the
+%     H&S test problem #71:
+%
+%         function H = hessian (x, sigma, lambda)
+%           H = sigma*[ 2*x(4)             0      0   0;
+%                       x(4)               0      0   0;
+%                       x(4)               0      0   0;
+%                       2*x(1)+x(2)+x(3)  x(1)  x(1)  0 ];
+%           H = H + lambda(1)*[    0          0         0         0;
+%                               x(3)*x(4)     0         0         0;
+%                               x(2)*x(4) x(1)*x(4)     0         0;
+%                               x(2)*x(3) x(1)*x(3) x(1)*x(2)     0  ];
+%           H = sparse(H + lambda(2)*diag([2 2 2 2]));
+%  
 %     funcs.hessianstructure (optional)
 % 
 %     This function serves the same purpose as funcs.jacobianstructure, but
 %     for the Hessian matrix. Again, it is not needed if you are using the
 %     limited-memory quasi-Newton approximation to the Hessian. It takes no
-%     inputs, and must return a sparse, lower triangular matrix.
+%     inputs, and must return a sparse, lower triangular matrix. For H&S
+%     test problem #71, the MATLAB callback routine is fairly
+%     straightforward:
+%
+%         function H = hessianstructure() 
+%           H = sparse(tril(ones(4)));
 %
 %     funcs.iterfunc (optional)
 %
@@ -111,7 +157,10 @@
 %     auxdata. The auxiliary data may not change through the course of the
 %     IPOPT optimization. The auxiliary data keep the same values as they
 %     possessed in the initial call. If you need variables that change over
-%     time, you may want to consider global variables (type HELP GLOBAL).
+%     time, you may want to consider global variables (type HELP
+%     GLOBAL). See the lasso.m file in the examples subdirectory for an
+%     illustration of how the auxiliary data is passed to the various
+%     callback functions.
 %
 %     options.zl, options.zu, options.lambda
 %
