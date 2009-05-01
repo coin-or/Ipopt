8211503a41e0a598a4e5136076893b4f7de8d171
Andreas Waechter <w4chtel@gmail.com>
2009-05-01 21:50:31 +0000

    set version number for release
    svn-id: 1447 @3.6

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.6stable"
+#define IPOPT_VERSION "3.6.1"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
