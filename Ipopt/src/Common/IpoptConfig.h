7495e2fc3bd86b633ae8ff567445cadcd497ede9
Andreas Waechter <andreas.waechter@northwestern.edu>
2009-10-29 22:29:37 +0000

    corrected Windows header version number

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.8.0"
+#define IPOPT_VERSION "3.8stable"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
