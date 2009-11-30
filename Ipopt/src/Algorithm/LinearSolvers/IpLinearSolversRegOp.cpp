6f4b789c93fc3342778a5c3c3494339894bc3602
Andreas Waechter <andreas.waechter@northwestern.edu>
2009-11-30 18:58:06 +0000

    bugfix ma77 configure

--- Ipopt/src/Algorithm/LinearSolvers/IpLinearSolversRegOp.cpp
+++ Ipopt/src/Algorithm/LinearSolvers/IpLinearSolversRegOp.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2005, 2008 International Business Machines and others.
+// Copyright (C) 2005, 2009 International Business Machines and others.
 // All Rights Reserved.
 // This code is published under the Common Public License.
 //
