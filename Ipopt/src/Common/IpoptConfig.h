4f66909f96a99376d3592d1651b9e779d31c062d
Andreas Waechter <w4chtel@gmail.com>
2010-11-05 15:58:23 +0000

    updated version numbers for creating new stable/3.9
    svn-id: 1802 @trunk

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.8trunk"
+#define IPOPT_VERSION "3.9trunk"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
