b2f0ce3a4d6e1cd104f8d0ead4d5910bb44eb908
Andreas Waechter <w4chtel@gmail.com>
2010-12-22 20:08:57 +0000

    temporarily committing release candidate
    svn-id: 1866 @3.9

--- Ipopt/src/Common/IpoptConfig.h
+++ Ipopt/src/Common/IpoptConfig.h
@@ -38,7 +38,7 @@
 /* include the COIN-wide system specific configure header */
 #include "configall_system.h"
 
-#define IPOPT_VERSION "3.9stable"
+#define IPOPT_VERSION "3.9.2"
 
 /***************************************************************************/
 /*             HERE DEFINE THE CONFIGURATION SPECIFIC MACROS               */
