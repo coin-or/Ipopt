5d27360057ef8b28da5e37f0be98ccb346957a2a
Hans Pirnay <hans.pirnay@rwth-aachen.de>
2011-04-01 07:46:32 +0000

    Allow for an unlimited number of steps in AsNMPC

--- Ipopt/contrib/AsNMPC/AmplSolver/ampl_nmpc.cpp
+++ Ipopt/contrib/AsNMPC/AmplSolver/ampl_nmpc.cpp
@@ -1,4 +1,4 @@
-// Copyright 2009 Hans Pirnay
+// Copyright 2009, 2011 Hans Pirnay
 // All Rights Reserved.
 // This code is published under the Eclipse Public License.
 //
@@ -22,127 +22,6 @@ int main(int argv, char**argc)
 							   app_ipopt->Options(),
 							   app_ipopt->RegOptions());
 
-  // prepare suffixes, or metadata ...
-  SmartPtr<AmplSuffixHandler> suffix_handler = new AmplSuffixHandler();
-  // Modified for warm-start from AMPL
-  suffix_handler->AddAvailableSuffix("ipopt_zL_out", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->AddAvailableSuffix("ipopt_zU_out", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->AddAvailableSuffix("ipopt_zL_in", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->AddAvailableSuffix("ipopt_zU_in", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->AddAvailableSuffix("parameter", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nominal_value", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->AddAvailableSuffix("perturbed_value", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  // Suffixes for NMPC
-  suffix_handler->AddAvailableSuffix("nmpc_state_0", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_1", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_2", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_3", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_4", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_5", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-  suffix_handler->AddAvailableSuffix("nmpc_state_6", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-
-  suffix_handler->AddAvailableSuffix("nmpc_init_constr", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Index_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_0", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_1", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_2", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_3", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_4", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_5", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_6", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_1", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_2", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_3", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_4", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_5", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_6", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_1", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_2", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_3", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_4", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_5", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_6", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_1_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_1_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_1_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_1_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_2_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_2_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_2_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_2_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_3_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_3_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_3_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_3_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_4_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_4_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_4_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_4_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_5_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_5_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_5_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_5_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_6_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_6_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_sol_state_6_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-  suffix_handler->
-    AddAvailableSuffix("nmpc_state_value_6_z_U", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
-
-  // for reduced hessian computation
-  suffix_handler->AddAvailableSuffix("red_hessian", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
-
-
   // Register AsNMPC options
   RegisterOptions_AsNMPC(app_ipopt->RegOptions());
   app_ipopt->Options()->SetRegisteredOptions(app_ipopt->RegOptions());
@@ -155,9 +34,55 @@ int main(int argv, char**argc)
     //printf("ampl_ipopt.cpp: Error in first Initialize!!!!\n");
     exit(-100);
   }
-
     
   app_ipopt->Initialize();
+
+  // prepare suffixes, or metadata ...
+  SmartPtr<AmplSuffixHandler> suffix_handler = new AmplSuffixHandler();
+  // Modified for warm-start from AMPL
+  suffix_handler->AddAvailableSuffix("ipopt_zL_out", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+  suffix_handler->AddAvailableSuffix("ipopt_zU_out", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+  suffix_handler->AddAvailableSuffix("ipopt_zL_in", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+  suffix_handler->AddAvailableSuffix("ipopt_zU_in", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+
+  suffix_handler->AddAvailableSuffix("parameter", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
+  suffix_handler->AddAvailableSuffix("nominal_value", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+  suffix_handler->AddAvailableSuffix("perturbed_value", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+
+  // Suffixes for NMPC
+  suffix_handler->AddAvailableSuffix("nmpc_init_constr", AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Index_Type);
+
+  int n_nmpc_steps = 0;
+  app_ipopt->Options()->GetIntegerValue("n_nmpc_steps",n_nmpc_steps,"");
+  std::string state;
+  std::string state_value;
+  std::string sol_state;
+  std::string sol_state_zL;
+  std::string sol_state_zU;
+  for (int k=0; k<n_nmpc_steps; ++k) {
+    state        = "nmpc_state_";
+    state_value  = "nmpc_state_value_";
+    sol_state    = "nmpc_sol_state_";
+    sol_state_zL = sol_state;
+    sol_state_zU = sol_state;
+    append_Index(state,k);
+    append_Index(state_value,k);
+    append_Index(sol_state,k);
+    append_Index(sol_state_zL,k);
+    append_Index(sol_state_zU,k);
+    sol_state_zL += "z_L";
+    sol_state_zU += "z_U";
+    suffix_handler->AddAvailableSuffix(state, AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
+    suffix_handler->AddAvailableSuffix(state_value, AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+    suffix_handler->AddAvailableSuffix(sol_state, AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+    suffix_handler->AddAvailableSuffix(sol_state, AmplSuffixHandler::Constraint_Source, AmplSuffixHandler::Number_Type);
+    suffix_handler->AddAvailableSuffix(sol_state_zL, AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+    suffix_handler->AddAvailableSuffix(sol_state_zU, AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+    // what's this good for??  suffix_handler->AddAvailableSuffix("nmpc_state_value_1_z_L", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Number_Type);
+  }
+
+  // for reduced hessian computation
+  suffix_handler->AddAvailableSuffix("red_hessian", AmplSuffixHandler::Variable_Source, AmplSuffixHandler::Index_Type);
 
   // Create AmplOptionsList for AsNMPC AMPL options
   SmartPtr<AmplOptionsList> ampl_options_list = new AmplOptionsList();
