85168f05cba69299271eeb1a6287fc5b6e9b6292
Tony Kelman <kelman@berkeley.edu>
2013-05-06 20:32:19 +0000

    r2191 broke examplelasso.m - it was using an undocumented interface where the iterate and gradient are cell arrays. Fix is simple, don't throw error for cell array output of gradient callback. If any elements of the cell array are the wrong data type, it will error a few lines later at Iterate grad(ptr)

--- Ipopt/contrib/MatlabInterface/src/callbackfunctions.cpp
+++ Ipopt/contrib/MatlabInterface/src/callbackfunctions.cpp
@@ -195,7 +195,7 @@ gradient callback function");
   // Get the output from the MATLAB callback function, which is the
   // value of the gradient of the objective function at x.
   mxArray* ptr = outputs[0];
-  if (!mxIsDouble(ptr) || mxIsComplex(ptr))
+  if (!mxIsCell(ptr) && (!mxIsDouble(ptr) || mxIsComplex(ptr)))
     throw MatlabException("The gradient callback must return a real double vector");
   if (mxIsSparse(ptr)) {
     // convert sparse gradient to full (simplest method, not fastest)
