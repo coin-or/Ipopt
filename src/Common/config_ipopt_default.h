78ba2816b8764263082388ad10c161fba817d90b
Stefan Vigerske <svigerske@gams.com>
2020-04-14 18:59:23 +0200

    fix IPOPT_VERSION define

--- src/Common/config_ipopt_default.h
+++ src/Common/config_ipopt_default.h
@@ -5,7 +5,7 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define IPOPT_VERSION      "3.13.1"
+#define IPOPT_VERSION      "3.13.2"
 
 /* Major Version number of project */
 #define IPOPT_VERSION_MAJOR      3
