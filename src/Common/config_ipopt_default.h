be85996f7f980df1126d231c7feaa013649f4fe8
Stefan Vigerske <svigerske@gams.com>
2020-10-16 17:00:41 +0200

    version number updates for 3.13.4

--- src/Common/config_ipopt_default.h
+++ src/Common/config_ipopt_default.h
@@ -5,16 +5,16 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define IPOPT_VERSION      "3.13.2"
+#define IPOPT_VERSION "3.13.4"
 
 /* Major Version number of project */
-#define IPOPT_VERSION_MAJOR      3
+#define IPOPT_VERSION_MAJOR 3
 
 /* Minor Version number of project */
-#define IPOPT_VERSION_MINOR     13
+#define IPOPT_VERSION_MINOR 13
 
 /* Release Version number of project */
-#define IPOPT_VERSION_RELEASE    3
+#define IPOPT_VERSION_RELEASE 4
 
 /* Define to the C type corresponding to Fortran INTEGER */
 #ifndef IPOPT_FORTRAN_INTEGER_TYPE
