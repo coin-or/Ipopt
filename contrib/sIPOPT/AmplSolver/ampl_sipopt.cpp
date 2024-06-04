d54cb8ec4a75820e0019d2fcf262c1614a4d2970
Stefan Vigerske <svigerske@users.noreply.github.com>
2024-06-04 12:07:37 +0200

    fix ampl_sipopt.cpp
    need to pass on argv

--- contrib/sIPOPT/AmplSolver/ampl_sipopt.cpp
+++ contrib/sIPOPT/AmplSolver/ampl_sipopt.cpp
@@ -13,7 +13,7 @@
 
 int main(
    int    /*argc*/,
-   char** /*argv*/
+   char** argv
 )
 {
    using namespace Ipopt;
@@ -123,7 +123,7 @@ int main(
                                     "Number of sensitivity steps");
 
    // create AmplSensTNLP from argc.
-   SmartPtr<TNLP> sens_tnlp = new SensAmplTNLP(ConstPtr(app_ipopt->Jnlst()), app_ipopt->RegOptions(), app_ipopt->Options(), argc, suffix_handler,
+   SmartPtr<TNLP> sens_tnlp = new SensAmplTNLP(ConstPtr(app_ipopt->Jnlst()), app_ipopt->RegOptions(), app_ipopt->Options(), argv, suffix_handler,
       false, ampl_options_list);
 
    app_sens->Initialize();
